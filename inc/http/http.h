526548352088a618aa1d21ea4cd7a1c6e4ec799f
rjf12138 <3161173206@qq.com>
2021-04-01 22:58:51 +0800

    2021年 04月 01日 星期四 22:58:51 CST

--- inc/http/http.h
+++ inc/http/http.h
@@ -2,6 +2,7 @@
 #define __HTTP_H_
 
 #include "basic_head.h"
+#include "http_def.h"
 #include "buffer/byte_buffer.h"
 
 namespace my_utils {
@@ -9,24 +10,28 @@ namespace my_utils {
 class HttpPtl {
 public:
     HttpPtl(void);
+    HttpPtl(ByteBuffer &data);
     virtual ~HttpPtl(void);
 
     int parser(ByteBuffer &data);
     int generate(ByteBuffer& data);
 
     int set_content(ByteBuffer& data);
-    int set_header(string method, int status, string reason_phrase);
+    int set_header(string method, int status);
     int set_header_option(const string &key, const string &value);
 
     int get_status(void);
     string get_method(void);
     string get_header_option(const string &key);
     int get_content(ByteBuffer &data);
+    
+private:
+    string get_status_phrase(int status);
 
 private:
     int status_;
     string method_;
-    string reason_phrase_;
+    ByteBuffer content_;
     map<string, string> header_;
 };
 
